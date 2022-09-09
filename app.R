1ad08f79ba8c50a5d7c65bc9ba912614599b8b01
anantadinath <vickey.coool@gmail.com>
2022-09-09 15:48:17 +0530

    Working Code

--- app.R
+++ app.R
@@ -1,12 +1,3 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-#
 library(ggplot2)
 library(shiny)
 library(plotly)
@@ -14,49 +5,58 @@ library(plotly)
 # Define UI for application that draws a histogram
 ui <- fluidPage(
 
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-    tags$script(src = "js/vue.global.min.js"),
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
+  # Application title
+  titlePanel("Old Faithful Geyser Data"),
+  tags$script(src = "js/vue.global.min.js"),
+  # Sidebar with a slider input for number of bins
+  sidebarLayout(
+    sidebarPanel(
+      sliderInput("bins",
+        "Number of bins:",
+        min = 1,
+        max = 50,
+        value = 30
+      )
+    ),
 
-        # Show a plot of the generated distribution
-        mainPanel(
-          htmltools::attachDependencies(
-            x = "distPlot",
-            value = htmlwidgets:::getDependency("plotly") 
-          ),
-           htmlTemplate(filename = "html/index.html"),
-           tags$script(src = "js/index.js")
-        )
+    # Show a plot of the generated distribution
+    mainPanel(
+      tagList(
+        htmltools::attachDependencies(
+          x = "distPlotly",
+          value = htmlwidgets:::getDependency("plotly")
+        ),
+        htmlTemplate(filename = "html/index.html")
+      ),
+      tags$script(src = "js/index.js")
     )
+  )
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-  
-  mainPlot <- reactive({
-    x    <- faithful[, 2]
+  main_plot <- reactive({
+    x <- faithful[, 2]
     bins <- seq(min(x), max(x), length.out = input$bins + 1)
     ggplt <- ggplot(as.data.frame(x), aes(x = x)) +
       geom_histogram() +
       theme_bw()
-    
+
     return(ggplt)
   })
 
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        return(mainPlot())
-    })
+  output$distPlot <- renderPlot({
+    # generate bins based on input$bins from ui.R
+    return(main_plot())
+  })
+
+  output$distPlotly <- renderPlotly({
+    # generate bins based on input$bins from ui.R
+    pltly <- main_plot() |>
+      ggplotly()
+    return(pltly)
+  })
 }
 
-# Run the application 
+# Run the application
 shinyApp(ui = ui, server = server)
