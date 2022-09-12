c747256aa3119713c5c563a0e1fb2e1d1650f9b0
anantadinath <vickey.coool@gmail.com>
2022-09-12 12:26:21 +0530

    Now vue Variables are accesible from global scope

--- www/js/index.js
+++ www/js/index.js
@@ -1,33 +1,31 @@
-$(document).ready(() => {
-  $(document).on('shiny:connected', function (event) {
-    Shiny.unbindAll();
-    const { createApp, ref, onMounted, nextTick } = Vue;
+$(document).on('shiny:connected', function (event) {
+  Shiny.unbindAll();
+  const { createApp, ref, onMounted, nextTick } = Vue;
 
-    const application = createApp({
-      delimiters: ['{%%', '%%}'],
-      setup() {
-        const message = ref('Hello Vue!');
-        const plotid = ref('distPlot');
-        const plotlyid = ref('distPlotly');
+  application = createApp({
+    delimiters: ['{%%', '%%}'],
+    setup() {
+      const message = ref('Hello Vue!');
+      const plotid = ref('distPlot');
+      const plotlyid = ref('distPlotly');
 
-        onMounted(() => {
-          nextTick(() => {
-            console.log('mounted');
-            Shiny.bindAll('#shiny-plot-output-distPlot');
-            Shiny.bindAll('#html-widget-output-distPlotly');
-          });
+      onMounted(() => {
+        nextTick(() => {
+          console.log('mounted');
+          Shiny.bindAll('#shiny-plot-output-distPlot');
+          Shiny.bindAll('#html-widget-output-distPlotly');
         });
+      });
 
-        return {
-          message,
-          plotid,
-          plotlyid,
-        };
-      },
-    });
-
-    const runApp = application.mount('#app');
-
-    Shiny.bindAll();
+      return {
+        message,
+        plotid,
+        plotlyid,
+      };
+    },
   });
+
+  runApp = application.mount('#app');
+
+  Shiny.bindAll();
 });
